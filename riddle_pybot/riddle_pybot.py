a5e5a72e73db4414ced92bd388d5aa31f26c0dd7
Sassafrass <stephen.davey@my.jcu.edu.au>
2019-06-28 16:10:13 +1000

    Update Docstrings.

--- riddle_pybot/riddle_pybot.py
+++ riddle_pybot/riddle_pybot.py
@@ -1,4 +1,5 @@
-""" Davvotts RiddleBot """
+""" Davvotts RiddleBot. Now, RiddleMaster.
+Plug in your own login to give it a go. Dictionary is yours to keep also."""
 
 import requests
 from riddlebot.riddle_pybot.auth import AUTH
@@ -17,7 +18,7 @@ ETAOIN = "ETAOIN" \
          "BVKJXQZ"
 
 class RiddleBot:
-
+    """ Github Noop Challenge Bot. """
     def __init__(self):
         # POST login to get started
         self.start = requests.post(DOMAIN + START, json={'login': AUTH_LOGIN}).json()
@@ -31,9 +32,11 @@ class RiddleBot:
         self.fetch_riddle()
 
     def solve_reverse(self):
+        """ Returns reversed riddle_text """
         return "".join(reversed(self.riddle_text))
 
     def solve_rotation(self):
+        """ Solves rotation cipher. Requires rotation shift, n. """
         text = self.riddle_text
         r = ""
         try:
@@ -45,6 +48,7 @@ class RiddleBot:
         return r
 
     def solve_caesar(self):
+        """ Solves caesar cipher based on key or brute. """
         text = self.riddle_text
         r = ''
         try:
@@ -66,14 +70,13 @@ class RiddleBot:
         return result
 
     def solve_vigenere(self):
+        """ Solves Vigenere cipher, untested outside of Github Noop"""
         try:
             riddleKey = self.riddle['riddleKey']
         except KeyError:
-            # Key Unknown
+            # Key Unknown - Find some keys!
             possible_answer = self.find_best_keys()
             return possible_answer
-            # Key is '4' elements, [1, 4, 8, 23]
-            # Call
 
         space_index = [i for i, char in enumerate(self.riddle_text) if char == " "]
         l = list("".join(self.riddle_text.split()))
@@ -91,6 +94,7 @@ class RiddleBot:
         return "".join(result)
 
     def fetch_riddle(self):
+        """ Github Noop getter """
         req = requests.get(DOMAIN + self.riddle_path)
         self.riddle = req.json()
         self.riddle_path = self.riddle['riddlePath']
@@ -99,6 +103,7 @@ class RiddleBot:
         print(self.riddle)
 
     def post_answer(self, answer):
+        """ Github Noop post, and refresh bot """
         req = requests.post(DOMAIN + self.riddle_path, json={"answer": answer})
         msg = req.json()
         print(msg)
@@ -115,32 +120,35 @@ class RiddleBot:
             pass
 
     def find_best_keys(self):
+        """ Requires idea of number of keys. See Kasiski factorisation method. """
+        # Replace with factorisation method of potential length keys
+        # Len of key is given in message
         text = "".join(self.riddle_text.split())
         len_key = [char for char in self.riddle['message'] if char.isnumeric()]
         len_key = int(len_key[0])
+
         # Split text into # sets for each supposed key
         sets = self.get_subset_strings(len_key, text)
         # Decrypt each set by -1 -> -25 (shift index of up_case)
-        # pass that into get_score; scores = {shift: score}
         scores = {}
         set_to_shifts = {}
         for s in sets:
             for shift in range(26):
                 # use shift to decrypt each set
                 test = "".join([up_case[(up_case.index(c) - shift)] for c in s])
-                score = self.get_freq_score_revised(test)
+                score = self.get_frequency_score(test)
                 scores[shift] = score
-            # Now get highgest scoring shifts for that segment,
+            #Get highgest scoring shifts for that segment,
             highest = max(v for v in scores.values())
             potential_letter_keys = [k for (k, v) in scores.items() if v == highest]
             set_to_shifts[s] = potential_letter_keys
 
-        # print(set_to_shifts)
         key_list = [k for k in set_to_shifts.values()]
         words_from_best_key = self.test_keys(key_list)
         return words_from_best_key
 
     def get_subset_strings(self, key_len, text):
+        """ Iterates along unbroken text every number of key elements """
         sets = []
         for i in range(key_len):
             s = "".join([char for char in text[i::key_len]])
@@ -148,9 +156,11 @@ class RiddleBot:
         return sets
 
     def take_first(self, item):
+        """ Getter for sort key=function"""
         return item[0]
 
-    def get_freq_score_revised(self, text):
+    def get_frequency_score(self, text):
+        """ Returns frequency score of letters in text. Indicates likelihood of of being real text """
         # Get letter count
         letter_to_freq = {l: 0 for l in up_case}
         for char in set(text):
@@ -165,6 +175,7 @@ class RiddleBot:
             else:
                 freq_to_letter[letter_to_freq[letter]].append(letter)
         # Sort value chars by by ETAOIN - IMPORTANT
+        # This sort balances out the frequency score
         for freq in freq_to_letter:
             freq_to_letter[freq].sort(key=ETAOIN.find, reverse=True)
             freq_to_letter[freq] = "".join(freq_to_letter[freq])
@@ -174,7 +185,7 @@ class RiddleBot:
         freq_tups.sort(key=self.take_first, reverse=True)
         # Extract string to count
         freq_order = "".join([v for k, v in freq_tups])
-        # Count
+        # Score frequency
         score = 0
         for char in ETAOIN[:6]:
             if char in freq_order[:6]:
@@ -185,18 +196,17 @@ class RiddleBot:
         return score
 
     def test_keys(self, key_list):
+        """ Takes list of lists. Iterates ordered combinations, uses key for vigenere, then scores if a match"""
         # Extract keys into list
-        # keys = [k for k in set_to_shifts.values()]
         keys = key_list
         # itertools Product to get combinations if necessary
         shifts = list(product(*keys))
 
         # Now have a list of potential keys to plug into vigenere
         for key in shifts:
-            wordiness = 0
             self.riddle['riddleKey'] = key
             test = self.solve_vigenere()
-            # print(test)
+            # Pass test into scoring, if high enough, return phrase
             if self.score_words(test) > 10:
                 return test
             elif self.score_words(test, reverse=True) > 10:
@@ -204,6 +214,7 @@ class RiddleBot:
         return None
 
     def score_words(self, words, reverse=False):
+        """ Scores text for number of words in Eng. dictionary """
         wordiness = 0
         w = words
         if reverse:
